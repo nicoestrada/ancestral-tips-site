471a7a266dc2f9e458971f0b2c3ba1698971616b
estradanicolas <estradanicolas@gmail.com>
2020-12-21 16:08:32 -0600

    fixed errors

--- src/templates/author.js
+++ src/templates/author.js
@@ -31,9 +31,9 @@ const Author = ({ data, location, pageContext }) => {
                             <h1>{author.name}</h1>
                             {author.bio && <p>{author.bio}</p>}
                             <div className="author-header-meta">
-                                {author.website && <a className="author-header-item" href={author.website} _blank" rel="noopener noreferrer">Website</a>}
-                                {twitterUrl && <a className="author-header-item" href={twitterUrl} _blank" rel="noopener noreferrer">Twitter</a>}
-                                {facebookUrl && <a className="author-header-item" href={facebookUrl} _blank" rel="noopener noreferrer">Facebook</a>}
+                                {author.website && <a className="author-header-item" href={author.website} target="_blank" rel="noopener noreferrer">Website</a>}
+                                {twitterUrl && <a className="author-header-item" href={twitterUrl} target="_blank" rel="noopener noreferrer">Twitter</a>}
+                                {facebookUrl && <a className="author-header-item" href={facebookUrl} target="_blank" rel="noopener noreferrer">Facebook</a>}
                             </div>
                         </div>
                         <div className="author-header-image">
